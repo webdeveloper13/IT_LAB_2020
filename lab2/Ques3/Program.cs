77aa9e96bea324fa4bb057bd4dcab7b4afdae9f1
Suryansh Sharma <42321091+webdeveloper13@users.noreply.github.com>
2020-12-01 16:05:39 +0530

    Add files via upload

---
+++ lab2/Ques3/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Ques3
+{
+    public delegate void TrafficDel();
+    class TrafficSignal
+    {
+        public void Red()
+        {
+            Console.WriteLine("RED MEANS STOP!!");
+        }
+
+        public void Yellow()
+        {
+            Console.WriteLine("YELLOW MEANS WAIT!!");
+        }
+
+        public void Green()
+        {
+            Console.WriteLine("GREEN MEANS GO!!");
+        }
+        static void Main(string[] args)
+        {
+            TrafficSignal ts = new TrafficSignal();
+            TrafficDel td;
+            td = ts.Red;
+            td();
+            td = ts.Yellow;
+            td();
+            td = ts.Green;
+            td();
+            //Console.WriteLine("Hello World!");
+        }
+    }
+}
