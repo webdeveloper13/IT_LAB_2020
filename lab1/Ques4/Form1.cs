f05af81adc42b7121b9074168df35d85cf9e9a24
Suryansh Sharma <42321091+webdeveloper13@users.noreply.github.com>
2020-12-01 16:03:52 +0530

    Add files via upload

---
+++ lab1/Ques4/Form1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Ques4
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (comboBox1.SelectedItem.ToString() != null || comboBox2.SelectedItem.ToString() != null)
+            {
+                MessageBox.Show("Thankyou for purchasing from us");
+            }
+
+            else
+            {
+                MessageBox.Show("Please select both model and colour of the car");
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            comboBox1.Text = "";
+            comboBox2.Text = "";
+        }
+
+    }
+}
